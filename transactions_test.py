ba98a81d6506ffdbe2c49ca4031c620e14a02224
Yanis TALEB <talebyanis.v@gmail.com>
2020-11-26 19:37:50 +0100

    Compte rendu TP bases de Données (Transactions) Binome: - TALEB Yanis - ATTRASSI Alaeddine v2
    Compte rendu TP bases de Données (Transactions)
    Binome:
     - TALEB Yanis
    - ATTRASSI Alaeddine

--- transactions_test.py
+++ transactions_test.py
@@ -1,22 +1,25 @@
 import pyodbc 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=LAPTOP-1MKAJNIV\MSSQLSERVER02;'
-                      'Database=transactions_test;'
-                      'Trusted_Connection=yes;')
-cursor = conn.cursor()
+try:
+    conn = pyodbc.connect('Driver={SQL Server};'
+                        'Server=LAPTOP-1MKAJNIV\MSSQLSERVER02;'
+                        'Database=transactions_test;'
+                        'Trusted_Connection=yes;')
+    cursor = conn.cursor()
 
-cursor.execute('select * from Fournisseurs')
-for row in cursor:
-    print(row)
+    cursor.execute('select * from Fournisseurs')
+    for row in cursor:
+        print(row)
 
-####################TRANSACTION####################
-cursor.execute("""update Fournisseurs set NomF = 'Jhon',VilleF = 'Nen-York', AdrF= 'BRONX',TelF = 'OOOOOOOO' where Idf = 4 """)
-cursor.execute("""update Fournisseurs set NomF = 'David',VilleF = 'los-Angeles',AdrF = 'beverly-hills',TelF = 'MMMMMMMM' where Idf = 5 """)
-cursor.execute("""update Fournisseurs set NomF = 'Lewis',VilleF = 'Tizi-Ouzou',AdrF = 'Boghni',TelF = 'PPPPPPPP'  where Idf = 6 """)
-conn.commit()
-cursor.execute('select * from Fournisseurs')
-print('______________________________________________________________')
+    cursor.execute("""update Fournisseurs set NomF = 'thomas',VilleF = 'Nice', AdrF= 'rue de nice',TelF = 'NNNNNNNN' where Idf = 7 """)
 
-for row in cursor:
-    print(row)
+    conn.commit()
+    cursor.execute('select * from Fournisseurs')
+
+    for row in cursor:
+        print(row)
+    #conn.close()
+except :
+    print("Database Update Failed !: {}")
+    conn.rollback()
+
 conn.close()
